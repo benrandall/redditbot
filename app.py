b4587a377f7ceb894fb7360682ac66dfa4009d78
Benjamin Randall <bengr35@gmail.com>
2018-02-13 13:45:45 -0600

    downloads top videos of day to /clips

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
-#   Copyright 2018
-
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import praw            # For reddit API
-import time            # For sleep function
-import sys             # For stderr message output
-import configparser    # For reading ini file for authentication
+# Imports
+import praw                 # For reddit API
+import time                 # For sleep function
+import sys                  # For stderr message output
+import configparser         # For reading ini file for authentication
+from pytube import YouTube  # For downloading YouTube videos
 
 # Read the config filea
 config = configparser.ConfigParser()
@@ -27,19 +14,33 @@ CLIENTID = config.get('redditbot', 'client_id')
 SECRET = config.get('redditbot', 'client_secret')
 USER_AGENT = config.get('redditbot', 'user_agent')
 
-def main():
 
+def main():
+    
 	# Initialize Reddit instance
-    bot = praw.Reddit(user_agent=USER_AGENT,
+    reddit = praw.Reddit(user_agent=USER_AGENT,
                       client_id=CLIENTID,
                       client_secret=SECRET,
                       username=USERNAME,
                       password=PASSWORD)
     print("Logged in.", file=sys.stderr)
 
-    subreddit = bot.subreddit('youtubehaiku')
-    comments = subreddit.stream.comments()
+    # Get top videos of the day
+    subreddit = reddit.subreddit('youtubehaiku')
+    limit = 25
+    i = 0
+    for submission in subreddit.top('day'):
+        if "[Announcement]" or "[NSFW]" not in submission.title:
+            url = submission.url
+            if "youtu" in url:
+                print("Downloading:", submission.title, file=sys.stderr)
+                yt = YouTube(submission.url)
+                stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
+                #TODO: check if stream is valid
+                stream.download('./clips/')
+        if i >= limit:
+            break
+        i = i + 1
 
-    # Fetch videos
 
 main()
